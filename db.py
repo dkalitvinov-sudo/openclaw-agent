b915e86605daf2c977ce38a76b621ed6ffb41118
dkalitvinov-sudo <dkalitvinov@gmail.com>
2026-03-27 10:48:31 +0200

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ from sqlalchemy import BigInteger, Boolean, DateTime, Integer, String, Text, sel
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-from app.config import get_settings
+from config import get_settings
 
 
 settings = get_settings()
